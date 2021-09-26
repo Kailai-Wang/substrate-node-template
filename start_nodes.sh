2e4779e4805c11e36f31f71ea9a013b51f94b1f1
Kailai Wang <Kailai.Wang@hotmail.com>
2021-09-26 14:47:04 +0200

    hardcode node-key so that everyone can start this script

--- start_nodes.sh
+++ start_nodes.sh
@@ -4,7 +4,7 @@
 ./target/release/node-template \
   --base-path /tmp/node01 \
   --chain=./node/res/homework_spec_raw.json \
-  --node-key-file=/tmp/node_key_01.txt \
+  --node-key=a58b84ac9dcb481d656bf8b5171396b05485f8dd39be2ea652a6a8f11755a724 \
   --port 30333 \
   --ws-port 9944 \
   --rpc-port 9933 \
@@ -15,7 +15,7 @@
 ./target/release/node-template \
   --base-path /tmp/node02 \
   --chain=./node/res/homework_spec_raw.json \
-  --node-key-file=/tmp/node_key_02.txt \
+  --node-key=be789a22b3d6616d6f836cc3bc55e82fe0fd04351b6997d94845a65ebc7e0129 \
   --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWPq12sbGQPFWHdgNVsQJpRboyNwFdAahkCK5iZNgiYr6A \
   --port 30334 \
   --ws-port 9945 \
